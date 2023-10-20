8136d199fd7ec91290d508df37e1e1434039a90a
Lucas Gomes <gomes.lucas@live.com>
2023-10-20 12:55:15 -0300

    added swagger

--- src/external/adapters/product/product.controller.ts
+++ src/external/adapters/product/product.controller.ts
@@ -11,26 +11,36 @@ import { CreateProductDto } from 'src/internal/domain/product/dto/create-product
 import { UpdateProductDto } from 'src/internal/domain/product/dto/update-product.dto';
 
 import { ProductsService } from './product.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
+  @ApiOperation({ summary: 'Create Product' })
+  @ApiResponse({ status: 201, description: 'Product successfully created.' })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({ summary: 'Update Product' })
+  @ApiResponse({ status: 201, description: 'Product successfully Updated.' })
   @Put()
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
+  @ApiOperation({ summary: 'Delete Product' })
+  @ApiResponse({ status: 201, description: 'Product successfully Deleted.' })
   @Delete()
   delete(@Param('id') id: string) {
     return this.productsService.delete(id);
   }
 
+  @ApiOperation({ summary: 'Get Product' })
+  @ApiResponse({ status: 200 })
   @Get('category/:category')
   findByCategory(@Param('category') category: string) {
     return this.productsService.findByCategory(category);
